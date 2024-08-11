b1226fcf68652eb8ea694b05e5fad9b4b2fb2ad2
Shubham Goyal <shubhamgoyal7125@gmail.com>
2024-08-11 18:56:38 +0530

    Chnaged profile pic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CTA from './CTA'
 import './header.css'
-import ME from '../../assets/me.png'
+import ME from '../../assets/portfolio_img-removebg-preview.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
